aaf61c00b07508b349c3987760da868e1e5c800d
idevwork <bizwork1114@outlook.com>
2020-01-13 13:27:05 -0400

    refactor: implement reselect

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,14 +1,18 @@
+import { createSelector } from 'reselect'
+
+export const getCart = state => state.cart
 export const getProducts = state => state.products
-
-export const getSelectedTotalNum = state =>
-  state.cart.selectedProducts.reduce((acc, cur) => acc + cur.selectedNum, 0)
-
 export const getSelectedProducts = state => state.cart.selectedProducts
 
-export const getSubTotalOfSelectedProducts = state =>
-  state.cart.selectedProducts
-    .reduce((acc, cur) => acc + cur.price * cur.selectedNum, 0)
-    .toFixed(2)
+export const getSelectedTotalNum = createSelector(getCart, item =>
+  item.selectedProducts.reduce((acc, cur) => acc + cur.selectedNum, 0)
+)
+
+export const getSubTotalOfSelectedProducts = createSelector(
+  getSelectedProducts,
+  item =>
+    item.reduce((acc, cur) => acc + cur.price * cur.selectedNum, 0).toFixed(2)
+)
 
 export const getPromoAmount = (state, subTotal) => {
   const { discounttype, amount } = state.cart.promoCode
@@ -28,11 +32,11 @@ export const getAllPrice = state => {
   return { subTotal, promoAmount, basketTotal }
 }
 
-export const getcheckoutCartStatus = state => {
-  switch (state.cart.msg) {
+export const getcheckoutCartStatus = createSelector(getCart, item => {
+  switch (item.msg) {
     case 'SUCCESS':
       return 'SUCCESS'
     default:
       return ''
   }
-}
+})
